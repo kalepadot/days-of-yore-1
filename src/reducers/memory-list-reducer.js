fdd0221c34befe7a31c196d995dfdccbf6642603
Benjamin Thom <jaminthom@gmail.com>
2020-05-05 11:14:43 -0700

    update src/reducers/memory-list-reducer.js
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>ech>

--- src/reducers/memory-list-reducer.js
+++ src/reducers/memory-list-reducer.js
@@ -2,7 +2,14 @@ import * as c from './../actions/ActionTypes';
 
 export default (state = {}, action) => {
 
+  const {id} = action;
 
-default:
-  return state;
+  switch (action.type) {
+    case c.DELETE_MEMORY:
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
 };
