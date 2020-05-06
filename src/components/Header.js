be9781cb69a1489825f00e0bfec1d72d461f045a
Brandan Sayarath <brandan@brandan.tech>
2020-05-06 11:39:19 -0700

    add react routing Co-authored-by: Patrick Delaney <prestwick97@gmail.com> Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com> Co-authored-by: jeremy kale padot <jeremy@padot.us> Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
-  
   return (
-    <div>
+      // <React.Fragment>
+    <div className="header">
       <h1>Days Of Yore</h1>
+      <Link to="/">Home</Link>
+      <Link to="/memorycontrol">Memory Lane</Link>
+      <Link to="/signin">Sign-in</Link>
+      <Link to="/contact">Contact</Link>
       <hr />
     </div>
+      // </React.Fragment>
   );
 }
 
---
+++ src/components/Signin.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Signin(){  
+  return (
+    <div className="signin">
+      <h1>Sign In Right Here yo</h1>
+      <input placeholder='Email' /><br />
+      <input placeholder='Password' /><br />
+      <button>Login!</button>
+    </div>
+  );
+}
+
+export default Signin
