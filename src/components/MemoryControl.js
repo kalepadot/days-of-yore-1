8f566977b2365f689e0bb3a229923ea7f7ff0d18
Benjamin Thom <jaminthom@gmail.com>
2020-05-07 10:17:28 -0700

    update src/components/MemoryControl.js to include  authorization import statement and conditionals within the render method
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- src/components/MemoryControl.js
+++ src/components/MemoryControl.js
@@ -3,7 +3,7 @@ import MemoryForm from './MemoryForm';
 import MemoryList from './MemoryList';
 import EditMemoryForm from './EditMemoryForm';
 import { connect } from 'react-redux';
-import { withFirestore } from 'react-redux-firebase';
+import { withFirestore, isLoaded } from 'react-redux-firebase';
 
 class MemoryControl extends React.Component {
 
@@ -62,39 +62,57 @@ class MemoryControl extends React.Component {
   }
 
   render() {
-    let currentlyVisibleState = null;
-    let buttonText = null;
-
-    if (this.state.editing) {
-      currentlyVisibleState = <EditMemoryForm memory={this.state.selectedMemory} onEdit={this.handleEditingMemoryInList} />;
-      buttonText = "Return to memory list";
-    } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <MemoryForm onMemoryCreation={this.handleAddingNewMemoryToList} />;
-      buttonText = "Return to memory list";
-    } else {
-      currentlyVisibleState = <MemoryList onMemorySelection={this.handleFlippingCard} onClickingDelete={this.handleDeletingMemory} onClickingEdit={this.handleEditClick} flippedOver={this.state.flippedOver} />;
-      buttonText = "Add memory";
+    const auth = this.props.firebase.auth();
+    if (!isLoaded(auth)) {
+      return (
+        <React.Fragment>
+          <h1>Loading...</h1>
+        </React.Fragment>
+      )
     }
+    if ((isLoaded(auth)) && (auth.currentUser == null)) {
+      return (
+        <React.Fragment>
+          <h1>Nah, sign in first bro!</h1>
+        </React.Fragment>
+      )
+    }
+    if ((isLoaded(auth)) && (auth.currentUser != null)) {
 
-    return (
-      <React.Fragment>
-        <div>
-          {currentlyVisibleState}
-        </div>
-        <div className="mainBody">
-          <button onClick={this.handleClick}>{buttonText}</button>
-        </div>
-      </React.Fragment>
-    );
+      let currentlyVisibleState = null;
+      let buttonText = null;
+  
+      if (this.state.editing) {
+        currentlyVisibleState = <EditMemoryForm memory={this.state.selectedMemory} onEdit={this.handleEditingMemoryInList} />;
+        buttonText = "Return to memory list";
+      } else if (this.state.formVisibleOnPage) {
+        currentlyVisibleState = <MemoryForm onMemoryCreation={this.handleAddingNewMemoryToList} />;
+        buttonText = "Return to memory list";
+      } else {
+        currentlyVisibleState = <MemoryList onMemorySelection={this.handleFlippingCard} onClickingDelete={this.handleDeletingMemory} onClickingEdit={this.handleEditClick} flippedOver={this.state.flippedOver} />;
+        buttonText = "Add memory";
+      }
+  
+      return (
+        <React.Fragment>
+          <div>
+            {currentlyVisibleState}
+          </div>
+          <div className="mainBody">
+            <button onClick={this.handleClick}>{buttonText}</button>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    formVisibleOnPage: state.formVisibleOnPage
-  }
-}
+// const mapStateToProps = state => {
+//   return {
+//     formVisibleOnPage: state.formVisibleOnPage
+//   }
+// }
 
-MemoryControl = connect(mapStateToProps)(MemoryControl);
+// MemoryControl = connect(mapStateToProps)(MemoryControl);
 
 export default withFirestore(MemoryControl);
